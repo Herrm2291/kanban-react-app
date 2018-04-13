e55dbb8af7f1321e697fe6c5dc83c41b5a8138b8
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-04-13 17:17:47 -0400

    added provider and libs folders and files within them

---
+++ app/components/App.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import uuid from 'uuid';
+import Notes from './Notes';
+import connect from '../libs/connect';
+import NoteActions from '../actions/NoteActions';
+
+
+class App extends React.Component {
+  render() {
+    const {notes} = this.props;
+
+    return (
+      <div>
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <Notes
+          notes={notes}
+          onNoteClick={this.activateNoteEdit}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote} />
+      </div>
+    );
+  }
+
+addNote = () => {
+  this.props.NoteActions.create({
+       id: uuid.v4(),
+       task: 'New task'
+     });
+}
+
+deleteNote = (id, e) => {
+  // Avoid bubbling to edit
+  e.stopPropagation();
+
+  this.setState({
+    notes: this.state.notes.filter(note => note.id !== id)
+  });
+}
+
+activateNoteEdit = (id) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = true;
+        }
+
+        return note;
+      })
+    });
+  }
+  editNote = (id, task) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = false;
+          note.task = task;
+        }
+
+        return note;
+      })
+    });
+  }
+}
+
+export default connect(({notes}) => ({
+  notes
+}), {
+  NoteActions
+})(App)
